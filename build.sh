18e4de4ed334801f6130e4fb18d7d6ef1f8e4ace
RNS135 <52278427+RNS135@users.noreply.github.com>
2019-08-28 15:40:10 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,12 @@
 # Upload Color App to Ledger Nano
 echo "***Downloading Submodules***"
 git submodule update --init --recursive
+echo "***Downloading Color App***"
+git submodule add https://github.com/RNSSolution/ledger-color-app
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+mv "$parent_path"/ledger-color-app/* "$parent_path"/src/ledger-user/
 echo "***Downloading Essential Modules"
-sudo apt-get update && sudo apt-get -y install build-essential git sudo wget cmake libssl-dev libgmp-dev autoconf libtool python-pip
+sudo apt-get update && sudo apt-get -y install build-essential git sudo wget cmake libssl-dev libgmp-dev$
 echo "***Downloading Modules of Python"
 make deps
 echo "***Installing Cmake"
@@ -11,12 +15,10 @@ sudo apt-get -y install cmake
 echo "***Installing C++ Files***"
 cmake -DDISABLE_DOCKER_BUILDS=ON . && make
 export GTEST_COLOR=1 && ctest -VV
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 echo "***Parent Path"
 echo "$parent_path"
-cp "$parent_path"/load_color/* "$parent_path"/
-
+echo "***Build***"
 make        # Builds both Cosmos and Tendermint apps
-
+cp "$parent_path"/load_color/* "$parent_path"/
 echo ***Loads Color App - ledger Nano App should be attached -***
-make load_cosmos         # Loads Color app       (for users)
+make load_color         # Loads Color app       (for users)
